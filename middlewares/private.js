032deadbb153c71faac7175c5b9d9fb81669ca33
Vivek Shelke <88998663+vivekshelke712@users.noreply.github.com>
2025-03-18 20:37:46 +0530

    Update middlewares

---
+++ middlewares/private.js
@@ -0,0 +1,13 @@
+exports.userProtected = asyncHandler(async (req, res, next) => {
+    const { auth } = req.cookies
+    if (!auth) {
+        return res.status(401).json({ message: "Non Cookie Found" })
+    }
+    jwt.verify(auth, process.env.JWT_KEY, (err, decode) => {
+        if (err) {
+            return res.status(401).json({ message: err.message || "JWT ERROR" })
+        }
+        req.body.userId = decode.userId
+        req.user = decode.userId
+        next()
+    })
